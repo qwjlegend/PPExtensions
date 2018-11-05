d610f3d1650d2f848b8cd6b5c139605887366766
weqian <weqian@paypal.com>
2018-11-05 10:54:53 -0800

    Remove Production Related Codes

---
+++ ppextensions/extensions/extension_logger/extension_logger.py
@@ -0,0 +1,21 @@
+import logging
+from pathlib import Path
+
+logger_name = "extension_logger"
+logger = logging.getLogger(logger_name)
+logger.setLevel(logging.DEBUG)
+
+log_path = str(Path.home()) + "/.extensionlog.log"
+fh = logging.FileHandler(log_path)
+fh.setLevel(logging.DEBUG)
+
+sh = logging.StreamHandler()
+sh.setLevel(logging.DEBUG)
+
+fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(fmt)
+
+fh.setFormatter(formatter)
+sh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(sh)
